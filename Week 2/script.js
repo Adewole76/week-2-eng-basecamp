b36a29057ea864c430e1d012b44e3f2fd4fd8039
YeezyTheDev <adewoleoluwasegun7@gmail.com>
2026-01-30 11:35:18 +0100

    checkbox and image click

--- Week 2/script.js
+++ Week 2/script.js
@@ -3,11 +3,12 @@ const taskNameInput = document.querySelector(".task-input");
 const taskContainer = document.querySelector(".task-container");
 const errorMessage = document.querySelector(".error-message");
 const deleteIcon = document.querySelector(".delete-icon");
-const markAsDone = document.getElementById("status-check");
+let markAsDone; 
 const tasksArray = [];
 const exampleObject = {
     name: "me", status: "active", index: 1
 };
+
 // add Tasks to tasksArray
 addBtn.addEventListener('click', function(){
  let taskName = taskNameInput.value;
@@ -22,29 +23,32 @@ addBtn.addEventListener('click', function(){
   const storedTaskList = localStorage.getItem('taskList')
   const retrieveTaskList = JSON.parse(storedTaskList);
   console.log(retrieveTaskList);
-  
   const taskItem =retrieveTaskList.map(task => `<div class="task-div">
   <section class="name-delete">
      <article>
-         <input id="status-check" type="checkbox">
+         <input class="status-check" type="checkbox">
           <p class="task-name">${task.name}</p>
      </article>
          <img class="delete-icon" src="icons8-trash.svg" alt= "trash icon">
  </section>
           <p class ="task-status">${task.status}</p>
   </div>`).join('');
+  
   console.log(taskItem);
   taskContainer.innerHTML = taskItem;
 errorMessage.classList.add('hidden');
-
-const markAsDone = document.getElementById("status-check");
-console.log(markAsDone);
-return markAsDone; 
  }else{
     console.log("write something in the task input");
     errorMessage.classList.remove('hidden')
  }
 })
+taskContainer.addEventListener('click',function(event){
+    if (event.target.checked){
+        console.log('a checkbox was clicked')
+    }else if(event.target.src){
+        console.log('an image was clicked')
+    }
+})
 // delete task
-console.log(markAsDone);
+// console.log(markAsDone);
 
