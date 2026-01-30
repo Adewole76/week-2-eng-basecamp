69f32a98c6b9116486be8b2a25ca50b678fc52e8
YeezyTheDev <adewoleoluwasegun7@gmail.com>
2026-01-30 11:02:46 +0100

    first commit

--- Week 2/script.js
+++ Week 2/script.js
@@ -3,7 +3,7 @@ const taskNameInput = document.querySelector(".task-input");
 const taskContainer = document.querySelector(".task-container");
 const errorMessage = document.querySelector(".error-message");
 const deleteIcon = document.querySelector(".delete-icon");
-const markAsDone = document.querySelector(".task-checkbox")
+const markAsDone = document.getElementById("status-check");
 const tasksArray = [];
 const exampleObject = {
     name: "me", status: "active", index: 1
@@ -24,17 +24,27 @@ addBtn.addEventListener('click', function(){
   console.log(retrieveTaskList);
   
   const taskItem =retrieveTaskList.map(task => `<div class="task-div">
-         <input class="task-checkbox" type="checkbox"> <p class="task-name">${task.name}</p>
+  <section class="name-delete">
+     <article>
+         <input id="status-check" type="checkbox">
+          <p class="task-name">${task.name}</p>
+     </article>
          <img class="delete-icon" src="icons8-trash.svg" alt= "trash icon">
+ </section>
           <p class ="task-status">${task.status}</p>
   </div>`).join('');
   console.log(taskItem);
   taskContainer.innerHTML = taskItem;
 errorMessage.classList.add('hidden');
+
+const markAsDone = document.getElementById("status-check");
+console.log(markAsDone);
+return markAsDone; 
  }else{
     console.log("write something in the task input");
     errorMessage.classList.remove('hidden')
  }
 })
-
+// delete task
+console.log(markAsDone);
 
